11b331e000a6030f1c715917840003e27fa2c9fd
CaptConrado <ottey001@gmail.com>
2014-04-15 21:23:06 -0700

    added the channel to user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin, :prospect]
   after_initialize :set_default_role, :if => :new_record?
 
+  has_many :channels
+
   def set_default_role
     self.role ||= :user
   end
