9557af2d812f6b185dc1a7a73a91f2d1f6172e7a
CaptConrado <ottey001@gmail.com>
2014-04-15 18:03:16 -0700

    added first name last name to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228010617) do
+ActiveRecord::Schema.define(version: 20140416000404) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20140228010617) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "role"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
